ca5b18c29b6053cb6b781920bc85d9960ff8b238
OmPrakash23 <mishraomm5000@gmail.com>
2026-02-15 10:24:08 +0530

    Create backend server setup in index.js
    Initialize backend server with environment configuration and MongoDB connection.

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+// This app is for running the backend server and importing the files purposes.
+
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+.then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+  });
+})
+.catch((err) => {
+console.error("MongoDB connection error!", err)
+process.exit(1);
+})
