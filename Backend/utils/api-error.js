fb58df1705c1d813e0df64d769e0f979407a30fd
OmPrakash23 <mishraomm5000@gmail.com>
2026-02-15 10:38:04 +0530

    Add ApiError class for error handling

--- Backend/utils/api-error.js
+++ Backend/utils/api-error.js
@@ -1,1 +1,25 @@
 
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.success = false;
+    this.message = message;
+    this.errors = errors;
+    this.data = null;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
